c7ffe9ecfa286f3698a1d70b365f6231efa8c0e9
elbines <141184135+elbines@users.noreply.github.com>
2024-01-10 22:56:31 +0100

    load and search works, chategory does not

--- frontend/src/pages/Cocktails.jsx
+++ frontend/src/pages/Cocktails.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import styles from './Cocktails.module.css';
 import { Text } from '../UI/Typography';
 
-
 export const Cocktails = () => {
+    const [totalCocktails, setTotalCocktails] = useState(0); // New state variable
     const [cocktails, setCocktails] = useState([]);
+    const [displayedCocktails, setDisplayedCocktails] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [selectedFilter, setSelectedFilter] = useState('');
+    const [itemsToDisplay, setItemsToDisplay] = useState(6); // Initial number of cocktails to display
 
     const filters = {
         'Occasion': ['Summer', 'Christmas', 'Fall', 'Spring', 'Halloween'],
@@ -25,11 +27,16 @@ export const Cocktails = () => {
         fetch(`https://cbc-uvko.onrender.com/cocktails${query ? '?' + query : ''}`)
             .then(response => response.json())
             .then(data => {
-                // Display only the last 6 cocktails if no filters are applied
-                setCocktails((!searchTerm && !selectedFilter) ? data.slice(0, 6) : data);
+                setCocktails(data);
+                setDisplayedCocktails(data.slice(0, itemsToDisplay));
+                setTotalCocktails(data.length); // Update total number of cocktails
             })
             .catch(error => console.error('Error fetching cocktails:', error));
-    }, [searchTerm, selectedFilter]);
+    }, [searchTerm, selectedFilter, itemsToDisplay]);
+
+    const loadMoreCocktails = () => {
+        setItemsToDisplay(prev => prev + 6);
+    };
 
     return (
         <div className={styles.wrapper}>
@@ -62,8 +69,7 @@ export const Cocktails = () => {
 
             <Text type="H1" className={styles.h1}>OUR RECENT COCKTAILS</Text>
             <div className={styles.gridContainer}>
-                {cocktails.map(cocktail => (
-
+                {displayedCocktails.map(cocktail => (
                     <div key={cocktail._id}>
                         {cocktail.imageUrl && (
                             <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
@@ -74,17 +80,107 @@ export const Cocktails = () => {
                     </div>
                 ))}
             </div>
+
+            {/* Conditionally render Load More Button */}
+            {displayedCocktails.length < totalCocktails && (
+                <div className={styles.loadMoreButtonContainer}>
+                    <button onClick={loadMoreCocktails} className={styles.loadMoreButton}>
+                        Load More
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
 
 
-{/* <Link to={`/cocktail/${cocktail._id}`} key={cocktail._id}>
-{cocktail.imageUrl && (
-    <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
-)}
-<Text type="H3" className={styles.h3}>{cocktail.name}</Text>
-<Text type="SbodyText" className={styles.SbodyText}>⏲️: {cocktail.prepTime} | 🌟: {cocktail.strength}</Text>
-<Text type="SbodyText" className={styles.SbodyText}>⚡: {cocktail.strength} | 🏷️ : {cocktail.tags.join(', ')}</Text>
-</Link>
-))} */}
+// import React, { useState, useEffect } from 'react';
+// import styles from './Cocktails.module.css';
+// import { Text } from '../UI/Typography';
+
+
+// export const Cocktails = () => {
+//     const [cocktails, setCocktails] = useState([]);
+//     const [searchTerm, setSearchTerm] = useState('');
+//     const [selectedFilter, setSelectedFilter] = useState('');
+
+//     const filters = {
+//         'Occasion': ['Summer', 'Christmas', 'Fall', 'Spring', 'Halloween'],
+//         'Main Liquor': ['Gin', 'Tequila', 'Vermouth']
+//     };
+
+//     useEffect(() => {
+//         let query = '';
+//         if (searchTerm) {
+//             query += `search=${encodeURIComponent(searchTerm)}`;
+//         }
+//         if (selectedFilter) {
+//             query += (query ? '&' : '') + `category=${encodeURIComponent(selectedFilter)}`;
+//         }
+
+//         fetch(`https://cbc-uvko.onrender.com/cocktails${query ? '?' + query : ''}`)
+//             .then(response => response.json())
+//             .then(data => {
+//                 // Display only the last 6 cocktails if no filters are applied
+//                 setCocktails((!searchTerm && !selectedFilter) ? data.slice(0, 6) : data);
+//             })
+//             .catch(error => console.error('Error fetching cocktails:', error));
+//     }, [searchTerm, selectedFilter]);
+
+//     return (
+//         <div className={styles.wrapper}>
+//             {/* Search Input */}
+//             <div className={styles.searchContainer}>
+//                 <input
+//                     type="text"
+//                     placeholder="Search for cocktails..."
+//                     value={searchTerm}
+//                     onChange={(e) => setSearchTerm(e.target.value)}
+//                 />
+//             </div>
+
+//             {/* Filter Dropdown */}
+//             <div className={styles.dropdowns}>
+//                 <select
+//                     value={selectedFilter}
+//                     onChange={(e) => setSelectedFilter(e.target.value)}
+//                 >
+//                     <option value="">Select Filter</option>
+//                     {Object.entries(filters).map(([category, values]) => (
+//                         <optgroup label={category} key={category}>
+//                             {values.map(value => (
+//                                 <option key={value} value={value}>{value}</option>
+//                             ))}
+//                         </optgroup>
+//                     ))}
+//                 </select>
+//             </div>
+
+//             <Text type="H1" className={styles.h1}>OUR RECENT COCKTAILS</Text>
+//             <div className={styles.gridContainer}>
+//                 {cocktails.map(cocktail => (
+
+//                     <div key={cocktail._id}>
+//                         {cocktail.imageUrl && (
+//                             <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
+//                         )}
+//                         <Text type="H3" className={styles.h3}>{cocktail.name}</Text>
+//                         <Text type="SbodyText" className={styles.SbodyText}>⏲️: {cocktail.prepTime} | 🌟: {cocktail.strength}</Text>
+//                         <Text type="SbodyText" className={styles.SbodyText}>⚡: {cocktail.strength} | 🏷️ : {cocktail.tags.join(', ')}</Text>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// };
+
+
+// {/* <Link to={`/cocktail/${cocktail._id}`} key={cocktail._id}>
+// {cocktail.imageUrl && (
+//     <img src={`https://cbc-uvko.onrender.com/${cocktail.imageUrl}`} alt={cocktail.name} className={styles.cocktailImage} />
+// )}
+// <Text type="H3" className={styles.h3}>{cocktail.name}</Text>
+// <Text type="SbodyText" className={styles.SbodyText}>⏲️: {cocktail.prepTime} | 🌟: {cocktail.strength}</Text>
+// <Text type="SbodyText" className={styles.SbodyText}>⚡: {cocktail.strength} | 🏷️ : {cocktail.tags.join(', ')}</Text>
+// </Link>
+// ))} */}
